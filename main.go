1a946d2072f8226cda4475d36a6909220d34264a
nudopnu <forsuse@live.de>
2024-10-23 06:34:33 +0200

    Added basic server

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import "net/http"
+
+func main() {
+	serveMux := http.NewServeMux()
+	server := http.Server{
+		Handler: serveMux,
+		Addr:    ":8080",
+	}
+	server.ListenAndServe()
+}
