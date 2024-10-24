367bd8fada237f5888a2ce43834905a6077adbf4
nudopnu <forsuse@live.de>
2024-10-24 15:15:20 +0200

    Added platform config

--- main.go
+++ main.go
@@ -15,11 +15,13 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
+	platform       string
 }
 
 func main() {
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatal(err)
@@ -28,6 +30,7 @@ func main() {
 	state := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             database.New(db),
+		platform:       platform,
 	}
 	serveMux := http.NewServeMux()
 	server := http.Server{
